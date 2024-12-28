6af5ff5f070fa776628247880448b126613092f6
Moaz <148714017+Moaz787@users.noreply.github.com>
2024-12-28 17:00:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,7 @@ app.get("/", (req, res) => {
 
 const startServer = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB_URL);
     console.log("Connected to MongoDB");
     app.listen(process.env.PORT, () => {
       console.log(`http://localhsot:${process.env.PORT}`);
