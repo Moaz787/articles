b8e0e1e59c3c646ed3b6a5b69700936b6e1e6b3b
Moaz <148714017+Moaz787@users.noreply.github.com>
2025-01-03 21:42:26 +0200

    Update article.routes.js

--- routes/article.routes.js
+++ routes/article.routes.js
@@ -1,8 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const router = express.Router();
-
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const {
   getArticles,
@@ -14,8 +11,8 @@ const {
 
 router.get("/articles/", getArticles);
 router.get("/articles/:id", getArticle);
-router.post("/articles/", urlencodedParser, createArticle);
-router.put("/articles/:id", urlencodedParser, updateArticle);
-router.delete("/articles/:id", urlencodedParser, deleteArticle);
+router.post("/articles/", createArticle);
+router.put("/articles/:id", updateArticle);
+router.delete("/articles/:id", deleteArticle);
 
 module.exports = router;
